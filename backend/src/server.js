cc6ef51bea54735c449cc5fdabc335f2ed520377
Kashyap297 <kashyap29700@gmail.com>
2024-09-23 22:19:32 +0530

    User Authentication Done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const dbConnection = require("./config/db");
 const Config = require("./config");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 const PORT = Config.PORT || 5000;
 
+// Middleware to parse incoming JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
 // dbConnection
 dbConnection();
+
+// api routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, (err) => {
   if (err) {
